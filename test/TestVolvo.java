4aeed578f6b712422c552c35a68dc23c05530f98
Alex04412 <Alex.tang20041212@gmail.com>
2023-11-09 15:51:45 +0100

    Brakes and Gas in Cars instead of volvo and saab

--- test/TestVolvo.java
+++ test/TestVolvo.java
@@ -1,3 +1,26 @@
-public class TestVolvo {
+import org.junit.Before;
+import org.junit.Test;
 
+import java.awt.*;
+
+import static junit.framework.TestCase.*;
+public class TestVolvo {
+    private Volvo240 volvo;
+    @Before
+    public void init(){
+        volvo = new Volvo240();
+    }
+    @Test
+    public void testGasAndBrake(){
+
+        for (int i = 0; i < 130; i ++){
+            volvo.gas(1);
+        }
+
+        volvo.gas(1);
+        volvo.brake(0.9);
+        System.out.println(volvo.currentSpeed);
+        assertTrue(volvo.currentSpeed<=volvo.enginePower);
+
+    }
 }

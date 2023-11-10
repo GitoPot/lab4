560fd32433278b87c1ba5184f49c0c0e87920ffc
Alex04412 <Alex.tang20041212@gmail.com>
2023-11-10 14:34:06 +0100

    Scania increment/decrement Speed implemented. Added gradiant to Scania with functions

--- Scania.java
+++ Scania.java
@@ -2,20 +2,37 @@ import java.awt.*;
 
 public class Scania extends Cars{
 
+    private double gradiant;
+
+
     public Scania(){
-
-        super(2,100, Color.BLUE, "ScaniaTruck");
+        super(2,100, Color.BLUE, "Scania");
+        gradiant=0;
     }
-
 
     @Override
     protected void incrementSpeed(double amount){
-
+        if(gradiant == 0){
+            currentSpeed = Math.min(getCurrentSpeed() + amount, getEnginePower());
+        }
     }
     @Override
     protected void decrementSpeed(double amount){
-
+        if(currentSpeed <= amount){
+            currentSpeed = 0;
+        }else{
+            currentSpeed = getCurrentSpeed() - amount;
+        }
     }
 
+
+    protected void changeGradiant(double newGradiant){
+        if (this.getCurrentSpeed() == 0 && newGradiant<=70 && newGradiant>=0){
+            gradiant=newGradiant;
+        }
+    }
+    public double getGradiant(){
+        return gradiant;
+    }
 }
 

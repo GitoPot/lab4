fcb5059cb0070e8d247c77d620a53292cb34ce2f
Alex04412 <Alex.tang20041212@gmail.com>
2023-11-10 14:03:03 +0100

    Scania

--- Scania.java
+++ Scania.java
@@ -1,3 +1,16 @@
-public class Scania {
-    //Kolla vad duktig Anders har varit
+import java.awt.*;
+
+public class Scania extends Cars{
+
+    public Scania(){
+        super(2,100,Color.BLUE,"ScaniaTruck");
+    }
+    @Override
+    protected void incrementSpeed(double amount){
+
+    }
+    @Override
+    protected void decrementSpeed(double amount){
+
+    }
 }

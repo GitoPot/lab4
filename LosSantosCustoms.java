d169effd99f19c533cec70e0c042e23684bd8fa3
GitoPot <102609722+GitoPot@users.noreply.github.com>
2023-11-15 09:05:25 +0100

    Start of composition and delegation

--- LosSantosCustoms.java
+++ LosSantosCustoms.java
@@ -1,7 +1,7 @@
 public class LosSantosCustoms implements GenericGarage{//uppgift 3
     // Detta är vår bilverkstads fil
 
-    // Det behövs skapa en gemensam fil som olika vekstäder kan "ärva" från genom composition, det går inte att ärva normalt.
+    // Det behövs skapa en gemensam fil som olika verkstäder kan "ärva" från genom composition, det går inte att ärva normalt.
     private final GenericGarage parent = new GenericGarage();  // Composition
 
     @Override

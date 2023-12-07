16f8ce13987845429a23cc2c9cc27535f25bea28
tomrex1 <73821407+tomrex1@users.noreply.github.com>
2023-12-07 10:05:33 +0100

    observer

--- Application.java
+++ Application.java
@@ -5,16 +5,9 @@ public class Application {
 
     public static void main(String[] args) {
         // Instance of this class
-        //VehicleModel model = initVehicleModel();
-        VehicleModel model = new VehicleModel();
-        model.addVehicle(new Volvo240());
-        model.addVehicle(new Saab95());
-        model.addVehicle(new Scania());
+        VehicleModel model = initVehicleModel();
+        VehicleView view = initVehicleView(model);
 
-
-        //VehicleView view = initVehicleView(model);
-        VehicleView view = new VehicleView("Frame", model);
-        model.addListener(view);
         VehicleController cc = new VehicleController(view, model);
 
         // Start a new view and send a reference of self
